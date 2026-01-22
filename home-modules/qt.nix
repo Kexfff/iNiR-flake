fa05f05c463df3ff4e4b63b90b8b4e28ed583286
kexfff <llexxass@gmail.com>
2026-01-22 21:53:05 +0300

    open with attept 2 fix

--- home-modules/qt.nix
+++ home-modules/qt.nix
@@ -9,6 +9,7 @@ let
   qsPackage = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
   maybeIcon = name: lib.optional (lib.attrByPath [ name ] null pkgs != null) (lib.attrByPath [ name ] null pkgs);
+  maybePkg = path: lib.optional (lib.attrByPath path null pkgs != null) (lib.attrByPath path null pkgs);
 
   qtImports = [
     pkgs.kdePackages.qtbase
@@ -46,6 +47,8 @@ in {
     home.sessionVariables = {
       XDG_DATA_DIRS =
         "${lib.makeSearchPath "share" (iconPkgs ++ [ customPkgs.material-symbols ])}:$XDG_DATA_DIRS";
+      XDG_CONFIG_DIRS =
+        "$HOME/.nix-profile/etc/xdg:/etc/profiles/per-user/$USER/etc/xdg:/run/current-system/sw/etc/xdg:$XDG_CONFIG_DIRS";
     };
     home.packages = [
       (pkgs.writeShellScriptBin "qs" ''
@@ -61,6 +64,7 @@ in {
       pkgs.kdePackages.kservice
       pkgs.desktop-file-utils
       pkgs.shared-mime-info
+    ] ++ maybePkg [ "kservice" ] ++ [
       pythonEnv
     ];
   };
